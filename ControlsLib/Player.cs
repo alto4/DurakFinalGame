4e5023c3a20d7ff32e38e0592446d2b46a2df5ff
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2021-04-06 13:47:20 -0400

    Created Player Class

---
+++ ControlsLib/Player.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlsLib
+{
+    /// <summary>
+    /// Class will be used for persistant statistics log
+    /// </summary>
+    class Player
+    {
+        //Private Variables
+        private static string playerName;
+        private static int playerWins;
+        private static int playerTies;
+        private static int playerLosses;
+
+        //Constructors
+        //Default Constructor
+        public Player() 
+        {
+            //For testing only
+            setPlayerName("Nick");
+            setPlayerWins(11);
+            setPlayerTies(3);
+            setPlayerLosses(5);
+        }
+        //Parameterized Constructor
+        public Player(string name, int wins, int ties, int losses)
+        {
+            setPlayerName(name);
+            setPlayerWins(wins);
+            setPlayerTies(ties);
+            setPlayerLosses(losses);
+        }
+        //Getters and Setters
+        public string getPlayerName()
+        {
+            return playerName;
+        }
+
+        public void setPlayerName(string name)
+        {
+            playerName = name;
+        }
+
+        public int getPlayerWins()
+        {
+            return playerWins;
+        }
+
+        public void setPlayerWins(int wins)
+        {
+            playerWins = wins;
+        }
+
+        public int getPlayerTies()
+        {
+            return playerTies;
+        }
+
+        public void setPlayerTies(int ties)
+        {
+            playerTies = ties;
+        }
+
+        public int getPlayerLosses()
+        {
+            return playerLosses;
+        }
+
+        public void setPlayerLosses(int losses)
+        {
+            playerLosses = losses;
+        }
+
+        //Public Methods
+        public override string ToString()
+        {
+            string tempString="";
+
+            tempString += "  " + getPlayerName() + ": Wins: " + getPlayerWins().ToString();
+            tempString += "  Ties: " + getPlayerTies().ToString() + "  Losses: " + getPlayerLosses().ToString();
+
+            return tempString;
+        }
+    }
+}
